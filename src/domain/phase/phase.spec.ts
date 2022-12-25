4c99e86d03585b74365bf83498cfb0e389d2b0f5
harunrst <harun5hy@outlook.com>
2022-12-25 12:13:06 +0100

    update domain unit tests

--- src/domain/phase/phase.spec.ts
+++ src/domain/phase/phase.spec.ts
@@ -3,31 +3,173 @@ import * as PhaseConstants from "./constants";
 import { Task } from "../task/task";
 
 describe("Phase", () => {
-  it("Phase should be created as locked if there is previous phase", () => {
+  it("Phase should be created as locked when there is previous phase", () => {
+    //arrange
     const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
-    expect(phase.isLocked).toBeFalsy();
 
+    //act
     const secondPhase = new Phase(
       PhaseConstants.DefaultPhases.Discovery,
       phase.name
     );
+
+    //assert
+    expect(phase.isLocked).toBeFalsy();
     expect(secondPhase.isLocked).toBeTruthy();
   });
 
-  it("addTask throws validation when the phase is locked", () => {
+  it("addTask should add task", () => {
+    //arramge
+    const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+    expect(phase.tasks.length).toBe(0);
+
+    //act
+    phase.addTask(new Task("oak"));
+
+    //assert
+    expect(phase.tasks.length).toBeGreaterThan(0);
+  });
+
+  it("addTask should sync phase status", () => {
+    //arrange
+    const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+    const task = new Task("oak");
+    phase.addTask(task);
+    phase.completeTask(task.id);
+    expect(phase.isDone).toBeTruthy();
+
+    //act
+    phase.addTask(new Task("oak"));
+
+    //assert
+    expect(phase.isDone).toBeFalsy();
+  });
+
+  it("completeTask should complete task", () => {
+    //arrange
+    const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+    const task = new Task("oak");
+    phase.addTask(task);
+
+    //act
+    phase.completeTask(task.id);
+
+    //assert
+    expect(phase.tasks[0].isDone).toBeTruthy();
+    expect(phase.isDone).toBeTruthy();
+  });
+
+  it("completeTask throws exception if the phase is locked", () => {
+    let errorMessage = "";
     try {
+      //arrange
       const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+      const task = new Task("oak");
+      phase.addTask(task);
       phase.lock();
-      phase.addTask(new Task("oak"));
+
+      //act
+      phase.completeTask(task.id);
     } catch (error) {
-      expect(error.message).toBe(PhaseConstants.Validations.PhaseIsLocked);
+      errorMessage = error.message;
+    } finally {
+      //assert
+      expect(errorMessage).toBe(PhaseConstants.Validations.PhaseIsLocked);
     }
   });
 
-  it("addTask should add task", () => {
+  it("completeTask throws exception if the task is not found", () => {
+    let errorMessage = "";
+    try {
+      //arrrange
+      const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+      const task = new Task("oak");
+      phase.addTask(task);
+
+      //act
+      phase.completeTask("");
+    } catch (error) {
+      errorMessage = error.message;
+    } finally {
+      //assert
+      expect(errorMessage).toBe(PhaseConstants.Validations.TaskNotFound);
+    }
+  });
+
+  it("undoTask should undo task", () => {
+    //arrange
     const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
-    expect(phase.tasks.length).toBe(0);
-    phase.addTask(new Task("oak"));
-    expect(phase.tasks.length).toBeGreaterThan(0);
+    const task = new Task("oak");
+    phase.addTask(task);
+    phase.completeTask(task.id);
+    expect(phase.tasks[0].isDone).toBeTruthy();
+    expect(phase.isDone).toBeTruthy();
+
+    //act
+    phase.undoTask(task.id);
+
+    //assert
+    expect(phase.tasks[0].isDone).toBeFalsy();
+    expect(phase.isDone).toBeFalsy();
+  });
+
+  it("undoTask throws exception if the phase is locked", () => {
+    let errorMessage = "";
+    try {
+      //arrange
+      const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+      const task = new Task("oak");
+      phase.addTask(task);
+      phase.lock();
+
+      //act
+      phase.undoTask(task.id);
+    } catch (error) {
+      errorMessage = error.message;
+    } finally {
+      //assert
+      expect(errorMessage).toBe(PhaseConstants.Validations.PhaseIsLocked);
+    }
+  });
+
+  it("undoTask throws exception if the task is not found", () => {
+    let errorMessage = "";
+    try {
+      //arrange
+      const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+      const task = new Task("oak");
+      phase.addTask(task);
+
+      //act
+      phase.undoTask("");
+    } catch (error) {
+      errorMessage = error.message;
+    } finally {
+      //assert
+      expect(errorMessage).toBe(PhaseConstants.Validations.TaskNotFound);
+    }
+  });
+
+  it("lock should lock the phase", () => {
+    //arrange
+    const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+
+    //act
+    phase.lock();
+
+    //assert
+    expect(phase.isLocked).toBeTruthy();
+  });
+
+  it("unlock should unlock the phase", () => {
+    //arrange
+    const phase = new Phase(PhaseConstants.DefaultPhases.Foundation);
+    phase.lock();
+
+    //act
+    phase.unlock();
+
+    //assert
+    expect(phase.isLocked).toBeFalsy();
   });
 });
--- src/domain/task/task.spec.ts
+++ src/domain/task/task.spec.ts
@@ -2,30 +2,50 @@ import { Task } from "./task";
 import * as TaskConstants from "./constants";
 
 describe("Task", () => {
-  it("Description validation", () => {
-    try {
-      new Task("aa");
-    } catch (error) {
-      expect(error.message).toBe(TaskConstants.Validations.Description);
-    }
-  });
-
   it("Task should be created successfully", () => {
+    //arrange
     const taksDesc = "oak";
+
+    //act
     const task = new Task(taksDesc);
+
+    //assert
     expect(!!task.id).toBeTruthy();
     expect(task.description).toBe(taksDesc);
   });
 
+  it("Task creation throw validation when description is not valid", () => {
+    let errorMessage = "";
+    try {
+      //arrange & act
+      new Task("aa");
+    } catch (error) {
+      errorMessage = error.message;
+    } finally {
+      //assert
+      expect(errorMessage).toBe(TaskConstants.Validations.Description);
+    }
+  });
+
   it("Task should be done successfully", () => {
+    //arrrange
     const task = new Task("oak");
+
+    //act
     task.complete();
+
+    //assert
     expect(task.isDone).toBeTruthy();
   });
 
   it("Task should be undo successfully", () => {
+    //arrange
     const task = new Task("oak");
+
+    //act
     task.undo();
+
+    //assert
     expect(task.isDone).toBeFalsy();
   });
 });
