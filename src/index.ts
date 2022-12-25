8686b98ce188101981c0ff978d734c97778be824
harunrst <harun5hy@outlook.com>
2022-12-25 23:00:33 +0100

    update todos

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,7 @@ SetupGraphqlServer(app);
 
 //todo: add e2e test
 //todo: add integration tests for app startup components
-//todo: implement error handling middlewares
+//todo: implement error handling middleware & error types
 //todo: implement logging solution project-wide
 //todo: dockerize the app, implement ci/cd. preferrably travis ci -> google clould container registry and cloud run
-//todo: apply better practices for graphql setup
 //todo: add readme about architectural approaches and business
