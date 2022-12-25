dcc8455de281908e558d72761f7b370db282d9aa
harunrst <harun5hy@outlook.com>
2022-12-25 23:51:44 +0100

    update todos

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ InitializeComponents();
 //app should be started at last
 SetupGraphqlServer(app);
 
-//todo: add e2e test
+//todo: add e2e test (unit & integration tests are covering very much already, no rush for this)
 //todo: add integration tests for app startup components
-//todo: implement error handling middleware & error types
+//todo: implement error handling middleware & error/exception types
 //todo: implement logging solution project-wide
-//todo: dockerize the app, implement ci/cd. preferrably travis ci -> google clould container registry and cloud run
+//todo: implement ci/cd. preferrably travis ci -> google clould container registry and cloud run
 //todo: add readme about architectural approaches and business
