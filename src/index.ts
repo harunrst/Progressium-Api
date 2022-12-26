b0447d8f91e00ebfc79fab5055895a5854977362
harunrst <harun5hy@outlook.com>
2022-12-26 16:30:41 +0100

    remove todos

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,3 @@ InitializeComponents();
 
 //app should be started at last
 StartGraphqlServer(app);
-
-//todo: add e2e test (unit & integration tests are covering very much already, no rush for this)
-//todo: add integration tests for app startup components
-//todo: implement error handling middleware & error/exception types
-//todo: implement logging solution project-wide
-//todo: add readme about architectural approaches and business
-//todo: add meaningful versions to deployments
